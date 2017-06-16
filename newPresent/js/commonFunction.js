03fd3b65070e5882b450e1a00f105bebe4c949e7
mental0925 <d26762268@gmail.com>
2017-06-16 16:00:24 +0800

    Update commonFunction.js

--- newPresent/js/commonFunction.js
+++ newPresent/js/commonFunction.js
@@ -7,8 +7,8 @@ var timeText;
 var dialog;
 var message;
 var player;
-var playerX = 2000;
-var playerY = 200;
+var playerX = 2130;
+var playerY = 245;
 
 function loadBasicElements(){
 	game.load.spritesheet('player', 'image/man.png', 30, 55);
